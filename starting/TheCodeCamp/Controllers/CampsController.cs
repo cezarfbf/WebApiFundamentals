3c4698fb6d6010aa115e6139aae84e3103b128a8
cezarfbf <cezarfbf@live.com>
2019-04-01 09:51:02 -0300

    chenging remote repo

---
+++ starting/TheCodeCamp/Controllers/CampsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace TheCodeCamp.Controllers
+{
+    public class CampsController : ApiController
+    {
+        public object Get()
+        {
+            return new { Name = "John Galles", Occupation = "Illusionist" };
+        }
+    }
+}

f7fb276c7db1c5e3644feda578dea85d0ba26a0e
cezarfbf <cezarfbf@live.com>
2019-04-03 08:31:53 -0300

    Ajusting talk controller e mapping profile

--- starting/TheCodeCamp/Controllers/CampsController.cs
+++ starting/TheCodeCamp/Controllers/CampsController.cs
@@ -77,6 +77,11 @@ namespace TheCodeCamp.Controllers
         {
             try
             {
+                if (await _repository.GetCampAsync(model.Moniker) != null)
+                {
+                    ModelState.AddModelError("Moniker", "Moniker in user");
+                }
+
                 if (ModelState.IsValid)
                 {
                     var camp = _mapper.Map<Camp>(model);
@@ -91,17 +96,65 @@ namespace TheCodeCamp.Controllers
 
                     } 
                 }
-                
             }
             catch (Exception e)
             {
                 return InternalServerError(e);
             }
-
-            return BadRequest();
-
+            return BadRequest(ModelState);
         }
 
+        [Route("{moniker}")]
+        public async Task<IHttpActionResult> Put(string moniker, CampModel model)
+        {
+            try
+            {
+                var camp = await _repository.GetCampAsync(moniker);
+
+                if (camp == null) return NotFound();
+
+                _mapper.Map(model, camp);
+
+                if (await _repository.SaveChangesAsync())
+                {
+                    return Ok(_mapper.Map<CampModel>(camp));
+                }
+                else
+                {
+                    return InternalServerError();
+                }
+            }
+            catch (Exception e)
+            {
+                return InternalServerError(e);
+            }
+        }
+
+        [Route("{moniker}")]
+        public async Task<IHttpActionResult> Delete(string moniker)
+        {
+            try
+            {
+                var camp = await _repository.GetCampAsync(moniker);
+
+                if (camp == null) return NotFound();
+
+                _repository.DeleteCamp(camp);
+
+                if (await _repository.SaveChangesAsync())
+                {
+                    return Ok();
+                }
+                else
+                {
+                    return InternalServerError();
+                }
+            }
+            catch (Exception e)
+            {
+                return InternalServerError(e);
+            }
+        }
 
     }
 }
--- starting/TheCodeCamp/Models/CampModel.cs
+++ starting/TheCodeCamp/Models/CampModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,15 @@ using TheCodeCamp.Data;
 namespace TheCodeCamp.Models
 {
     public class CampModel
-    {        
+    {   
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Moniker { get; set; }
+        [Required]
         public DateTime EventDate { get; set; } = DateTime.MinValue;
+        [Required]
+        [Range(1,30)]
         public int Length { get; set; } = 1;
 
         public ICollection<TalkModel> Talks { get; set; }
